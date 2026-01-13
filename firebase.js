05c06de9805526560eb196ea966f2a675d31e1e6
jat4 <144493972+jat4@users.noreply.github.com>
2026-01-13 18:56:13 +0530

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,20 @@
+<!-- firebase.js -->
+<script src="https://www.gstatic.com/firebasejs/9.23.0/firebase-app-compat.js"></script>
+<script src="https://www.gstatic.com/firebasejs/9.23.0/firebase-auth-compat.js"></script>
+<script src="https://www.gstatic.com/firebasejs/9.23.0/firebase-database-compat.js"></script>
+
+<script>
+  const firebaseConfig = {
+    apiKey: "AIzaSyBRFCf1NGzRibFY0u72x1iGEuyahpyw4XI",
+    authDomain: "quiz-maker-a.firebaseapp.com",
+    projectId: "quiz-maker-a",
+    storageBucket: "quiz-maker-a.firebasestorage.app",
+    messagingSenderId: "1065580063682",
+    appId: "1:1065580063682:web:315a87ea265be8d139445b"
+  };
+
+  firebase.initializeApp(firebaseConfig);
+
+  const auth = firebase.auth();
+  const db = firebase.database();
+</script>
